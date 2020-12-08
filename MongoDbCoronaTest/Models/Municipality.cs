00b3a36cfe35d9013098c97073711b48f287db8b
hyggenygge <ketul@hafnet.dk>
2020-12-08 14:52:48 +0100

    Seeding af data

--- MongoDbCoronaTest/Models/Municipality.cs
+++ MongoDbCoronaTest/Models/Municipality.cs
@@ -17,6 +17,16 @@ namespace MongoDbCoronaTest.Models
             Location_id = new List<int>();
             TestCenter_id = new List<int>();
         }
+        public Municipality(int id, string name, int population, int locationId)
+        {
+            MunicipalityId = id;
+            Name = name;
+            Population = population;
+            Location_id = new List<int>();
+            Location_id.Add(locationId);
+            TestCenter_id = new List<int>();
+        }
+
         public ObjectId _id { get; set; }
         public int MunicipalityId { get; set; }
         public string Name { get; set; }
